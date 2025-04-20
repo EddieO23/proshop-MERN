a1ca43d300a47216fa1e9de310e20186c7a63e17
Eddie <eocampo52@gmail.com>
2025-04-20 14:40:09 -0500

    succesfully rendering product data to HomePage.

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import {Card} from 'react-bootstrap'
 import {Link} from 'react-router-dom'
 
+import Rating from './Rating'
+
 function Product({product}) {
   return (
     <Card className='my-3 p-3 rounded'>
@@ -10,11 +12,14 @@ function Product({product}) {
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
-        <Card.Title as='div'>
+        <Card.Title as='div' className='product-title'>
           <strong>{product.name}</strong>
         </Card.Title>
         </Link>
 
+        <Card.Text as='div'>
+          <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+        </Card.Text>
         <Card.Text as="h3">
           ${product.price}
         </Card.Text>
