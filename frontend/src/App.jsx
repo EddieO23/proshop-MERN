5bf54743c615b91fd457f909ffa3a2e305bbda8a
Eddie <eocampo52@gmail.com>
2025-04-20 14:21:08 -0500

    added react router dom to website project. added a specific version of react router dom to website. something about compatibility issues. i checked the course q&a for the answer. said to use a specific version of react router dom.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './Pages/HomeScreen';
 
 function App() {
   return (
@@ -11,7 +13,7 @@ function App() {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to Proshop!</h1>
+        <Outlet/>
         </Container>
       </main>
       <Footer/>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import 'bootstrap/dist/css/bootstrap.min.css'
-import './assets/styles/bootstrap.custom.css'
-import './assets/styles/index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom';
+
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
+import App from './App.jsx';
+import HomeScreen from './Pages/HomeScreen.jsx';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route index={true} path='/' element={<HomeScreen/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+);
