450903037094580fff3703c56f7d5c302de38c12
Eddie <eocampo52@gmail.com>
2025-04-21 15:09:08 -0500

    finished the frontend? worked on the productsPage component.

--- frontend/src/Pages/ProductPage.jsx
+++ frontend/src/Pages/ProductPage.jsx
@@ -12,6 +12,7 @@ function ProductPage() {
 
   return (
     <>
+      <h1>Welcome to the products page!</h1>
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
@@ -25,11 +26,42 @@ function ProductPage() {
               <h3>{product.name}</h3>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
             </ListGroup.Item>
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+            <ListGroup.Item>Description: {product.description}</ListGroup.Item>
           </ListGroup>
         </Col>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In Stock' : 'Out of Stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button className='btn-block'type='button' disabled={product.countInStock === 0}>
+                  Add to cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
       </Row>
     </>
   );
