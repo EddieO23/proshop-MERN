6b7c07946785e83719e64906ff9b294292e9e8e9
Eddie <eocampo52@gmail.com>
2025-04-24 13:50:33 -0500

    Working on the cart page
    Working on cart page. left on form control group to select the qty selector.

---
+++ frontend/src/Pages/CartPage.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  Row,
+  Col,
+  ListGroup,
+  Image,
+  Form,
+  Button,
+  Card,
+} from 'react-bootstrap';
+import { FaTrash } from 'react-icons/fa';
+
+import Message from '../components/Message';
+
+function CartPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1 style={{ marginBottom: '20px' }}>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your Cart is emty.
+            <Link to={'/'}>Go back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item._id}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item._id}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>${item.price}</Col>
+                  <Col md={2}>
+                  <FormControl as="select" value={item.qty} onChange={(e) => setQty(Number(e.target.value))}>
+                    {[...Array(product.countInStock).keys()].map((x) => (
+                      <option key={x + 1} value={x + 1}>
+                        {x + 1}
+                      </option>
+                    ))}
+                    </FormControl>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+    </Row>
+  );
+}
+
+export default CartPage;
