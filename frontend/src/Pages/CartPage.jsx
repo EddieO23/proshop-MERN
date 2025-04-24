c3a983599b14ad26ff0e3ae117aebc38d0acaaf4
Eddie <eocampo52@gmail.com>
2025-04-24 14:41:28 -0500

    Finished Cart functionality
    Added remove cart slice to our cartSlice. Added our handler functions for cart functionality. Finished the shopping cart screen.

--- frontend/src/Pages/CartPage.jsx
+++ frontend/src/Pages/CartPage.jsx
@@ -6,13 +6,14 @@ import {
   Col,
   ListGroup,
   Image,
-  Form,
+  FormControl,
   Button,
   Card,
 } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 
 import Message from '../components/Message';
+import { addToCart, removeFromCart } from '../slices/cartSlice';
 
 function CartPage() {
   const navigate = useNavigate();
@@ -20,6 +21,18 @@ function CartPage() {
 
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
+
+  const addToCartHandler = async (product, qty) => {
+    dispatch(addToCart({...product, qty}))
+  }
+
+  const removeFromCartHandler = async (id) => {
+    dispatch(removeFromCart(id))
+  }
+
+  const checkoutHandler =  () => {
+    navigate('/login?redirect=/shipping')
+  }
 
   return (
     <Row>
@@ -43,19 +56,45 @@ function CartPage() {
                   </Col>
                   <Col md={2}>${item.price}</Col>
                   <Col md={2}>
-                  <FormControl as="select" value={item.qty} onChange={(e) => setQty(Number(e.target.value))}>
-                    {[...Array(product.countInStock).keys()].map((x) => (
-                      <option key={x + 1} value={x + 1}>
-                        {x + 1}
-                      </option>
-                    ))}
+                    <FormControl
+                      as='select'
+                      value={item.qty}
+                      onChange={(e) => addToCartHandler(item, Number(e.target.value))}
+                    >
+                      {[...Array(item.countInStock).keys()].map((x) => (
+                        <option key={x + 1} value={x + 1}>
+                          {x + 1}
+                        </option>
+                      ))}
                     </FormControl>
+                  </Col>
+                  <Col md={2}>
+                  <Button type='button' variant='light' onClick={() => {
+                    removeFromCartHandler(item._id)
+                  }}>
+                    <FaTrash/>
+                  </Button>
                   </Col>
                 </Row>
               </ListGroup.Item>
             ))}
           </ListGroup>
         )}
+      </Col>
+      <Col md={4}>
+      <Card>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <h2>Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items</h2>
+            ${cartItems.reduce((acc, item) => acc + item.qty * item.price, 0 ).toFixed(2)}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Button onClick={checkoutHandler} type='button' className='btn-block' disabled={cartItems.length === 0}>
+              Proceed to checkout
+            </Button>
+          </ListGroup.Item>
+        </ListGroup>
+      </Card>
       </Col>
     </Row>
   );
