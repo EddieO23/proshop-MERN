721705c1465b0f087540dc1341a5f1a3d939f532
Eddie <eocampo52@gmail.com>
2025-04-25 13:54:29 -0500

    Working on backend-authentication
    Added the new userRoutes and userController for backend. Route direction for when hitting the api routes

---
+++ backend/controllers/userController.js
@@ -0,0 +1,77 @@
+import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/usertModel.js';
+
+// @desc AUTH User && get Token
+// @route POST /api/users/login
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  res.send('Auth User');
+});
+
+// @desc Register User
+// @route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.send('Register User');
+});
+
+// @desc Logout the User / clear cache
+// @route POST /api/users/logout
+// @access Private
+const logoutUser = asyncHandler(async (req, res) => {
+  res.send('Logout User');
+});
+
+// @desc Get User Profile
+// @route GET /api/users/profile
+// @access Public
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.send('GET User Profile');
+});
+
+// @desc Update User Profile
+// @route PATCH /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.send('Upate User Profile');
+});
+
+// @desc Get users
+// @route GET /api/users
+// @access Private/Admin
+const getUsers = asyncHandler(async (req, res) => {
+  res.send('Get Users');
+});
+
+// @desc Get user by ID
+// @route GET /api/users/:id
+// @access Admin
+const getUserById = asyncHandler(async (req, res) => {
+  res.send('Get User by ID');
+});
+
+// @desc Update User
+// @route PUT /api/users/:id
+// @access Private/Admin
+const updateUser = asyncHandler(async (req, res) => {
+  res.send('update user');
+});
+
+// @desc Detelete Users
+// @route DELETE /api/users/:id
+// @access Admin
+const deleteUser = asyncHandler(async (req, res) => {
+  res.send('delete user');
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
