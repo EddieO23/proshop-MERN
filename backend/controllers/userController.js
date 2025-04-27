81579f213e81f0ab279926d0b7033aa8b3b74ca4
Eddie <eocampo52@gmail.com>
2025-04-27 10:53:47 -0500

    Finished Backend-Authentication
    Successfully finished the backend authentication part of our web project.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,39 +1,27 @@
-import jwt from 'jsonwebtoken'
-
 import asyncHandler from '../middleware/asyncHandler.js';
 import User from '../models/userModel.js';
+import generateToken from '../utils/generateToken.js';
 
 // @desc AUTH User && get Token
 // @route POST /api/users/login
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-  const {email, password} = req.body
+  const { email, password } = req.body;
 
-  const user = await User.findOne({email})
+  const user = await User.findOne({ email });
 
-  if(user && (await user.matchPassword(password))) {
-    const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {
-      expiresIn: '30d'
-    })
-    
-    // set JWT as HTTP-ONLY Cookie
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
 
-    res.cookie('jwt', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV !== 'development',
-      sameSite: 'strict',
-      maxAge : 30 * 24 * 60 *60 * 1000 // 30days
-    })
-
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
-      isAdmin: user.isAdmin
-    })
+      isAdmin: user.isAdmin,
+    });
   } else {
-    res.status(401)
-    throw new Error('Invalid email or password')
+    res.status(401);
+    throw new Error('Invalid email or password');
   }
 });
 
@@ -41,28 +29,93 @@ const authUser = asyncHandler(async (req, res) => {
 // @route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.send('Register User');
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists.');
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data.');
+  }
 });
 
-// @desc Logout the User / clear cache
+// @desc LOGOUT the User / clear cache
 // @route POST /api/users/logout
 // @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('Logout User');
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ msg: 'Bye bye. See you next time.' });
 });
 
 // @desc Get User Profile
 // @route GET /api/users/profile
 // @access Public
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('GET User Profile');
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404)
+    throw new Error('User not found.')
+  }
 });
 
 // @desc Update User Profile
 // @route PATCH /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('Upate User Profile');
+  const user = await User.findById(req.user._id);
+
+  if(user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+
+    if(req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin
+    })
+  } else {
+    res.stauts(404) 
+    throw new Error('User not found.')
+  }
 });
 
 // @desc Get users
