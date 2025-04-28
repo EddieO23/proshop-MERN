e34a706ee5e46bbe0e9229fdae3dfe8d1bcd2e54
Eddie <eocampo52@gmail.com>
2025-04-28 15:31:27 -0500

    Added Private route component
    Added this route for only seeing shipping route if user is logged in.  otherwise get fucked.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,6 +13,7 @@ import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
 import App from './App.jsx';
 
+import PrivateRoute from './components/PrivateRoute.jsx';
 import HomeScreen from './Pages/HomeScreen.jsx';
 import CartPage from './Pages/CartPage.jsx';
 import ProductPage from './Pages/ProductPage.jsx';
@@ -28,7 +29,10 @@ const router = createBrowserRouter(
       <Route path='/cart' element={<CartPage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/register' element={<RegisterPage />} />
-      <Route path='/shipping' element={<ShippingPage />} />
+
+      <Route path='' element={<PrivateRoute />}>
+        <Route path='/shipping' element={<ShippingPage />} />
+      </Route>
     </Route>
   )
 );
