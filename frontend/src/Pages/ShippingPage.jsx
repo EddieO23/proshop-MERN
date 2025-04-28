4f81f03193dcd50c859f868e6c18e2c5e2ccb066
Eddie <eocampo52@gmail.com>
2025-04-28 15:41:33 -0500

    Checkout Steps
    Added checkout steps component to indicate where in the payment process we are in. they are displayed as nav links using react router and react bootstrap along as react router bootstrap for the linkcontainer

--- frontend/src/Pages/ShippingPage.jsx
+++ frontend/src/Pages/ShippingPage.jsx
@@ -5,6 +5,7 @@ import {useDispatch, useSelector} from 'react-redux'
 import {useNavigate, } from 'react-router-dom'
 
 
+import CheckoutSteps from '../components/CheckoutSteps';
 import {saveShippingAddress} from '../slices/cartSlice'
 import FormContainer from '../components/FormContainer';
 
@@ -30,6 +31,8 @@ const ShippingPage = () => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2/>
+      
       <h1>Shipping</h1>
 
       <Form onSubmit={submitHandler}>
