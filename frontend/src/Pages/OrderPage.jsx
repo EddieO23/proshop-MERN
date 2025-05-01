40b6522f5bbfaef8bb8e8c79db9c156d92f06356
Eddie <eocampo52@gmail.com>
2025-04-30 20:36:11 -0500

    Successfully integrating PayPal API to our MERN project!
    Added route for backend order controller smh never added the logic. Need to add the remaining controller logic too.
    Added function logic to buttons

--- frontend/src/Pages/OrderPage.jsx
+++ frontend/src/Pages/OrderPage.jsx
@@ -64,10 +64,40 @@ const OrderPage = () => {
     }
   }, [order, paypal, paypalDispatch, loadingPay, errorPayPal]);
 
-  function onApprove() {}
-  function onApproveTest() {}
-  function onError() {}
-  function createOrder() {}
+  function onApprove(data, actions) {
+    return actions.order.capture().then(async function (details) {
+      try {
+        await payOrder({ orderId, details });
+        refetch();
+        toast.success('Payment Succesful');
+      } catch (err) {
+        toast.error(err?.data?.message || err.message);
+      }
+    });
+  }
+  async function onApproveTest() {
+    await payOrder({ orderId, details: { payer: {} } });
+    refetch();
+    toast.success('Payment Succesful');
+  }
+  function onError(err) {
+    toast.error(err.message);
+  }
+  function createOrder(data, actions) {
+    return actions.order
+      .create({
+        purchase_units: [
+          {
+            amount: {
+              value: order.totalPrice,
+            },
+          },
+        ],
+      })
+      .then((orderId) => {
+        return orderId;
+      });
+  }
 
   return isLoading ? (
     <Loader />
@@ -172,12 +202,12 @@ const OrderPage = () => {
                     <Loader />
                   ) : (
                     <div>
-                      <Button
+                      {/* <Button
                         onClick={onApproveTest}
                         style={{ marginBottom: '10px' }}
                       >
                         Test Button
-                      </Button>
+                      </Button> */}
                       <div>
                         <PayPalButtons
                           createOrder={createOrder}
